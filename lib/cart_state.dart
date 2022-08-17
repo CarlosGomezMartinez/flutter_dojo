4b0691210e5b28df2823ab9efd1c2538523028ab
CarlosGomezMartinez <carlos.gomez35@udea.edu.co>
2022-08-16 22:18:47 -0500

    clase 16-08-22

---
+++ lib/cart_state.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dojo/product.model.dart';
+ 
+class CartState with ChangeNotifier {
+  List<Product> items = [];
+ 
+  addProduct(Product product) {
+    if (items.contains(product)) {
+      return;
+    }
+    items.add(product);
+    notifyListeners();
+  }
+}
